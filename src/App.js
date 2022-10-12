a0cb27dbabc0d0e83d796a77111bb522de7ad7d9
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-10-12 19:34:09 -0400

    made a nonfunctional button that says hi bc i am the best to ever do it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <button>hi</button>
     </div>
   );
 }
