c8c9d0659aa16c5ba774ec05691ac05595af89c8
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-10-14 16:49:47 -0400

    more code consistency

--- src/helper/routes.jsx
+++ src/helper/routes.jsx
@@ -1,7 +1,7 @@
 import {
     createBrowserRouter,
     RouterProvider
-} from "react-router-dom";
+} from 'react-router-dom';
 
 import Home from '../components/Home';
 import Login from '../components/Login';
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import App from './App';
 import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
