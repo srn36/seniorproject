fa2e3f5ecada522b2eda1a7fea2016e004151cd0
srn36 <srn36@case.edu>
2022-10-19 19:31:07 -0400

    hi

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -8,22 +8,23 @@ import { Navigate } from "react-router-dom/dist";
 import { DropdownButton } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import smallLogo from '../smollogo.png';
 
 function NavigationBar(props) {
     const logOut = () => {
         sessionStorage.removeItem('token');
         return props.setContent(<Navigate to='/login/' />);
     };
-
+//style={{display: 'flex'}}
     return (
         <div>
-            <DropdownButton id="dropdown-basic-button" title="Navigation" style={{display: 'flex'}}>
+            <DropdownButton id="dropdown-basic-button" title="Navigation">
                 <Link to='/' className='dropdown-item'>Home</Link>
                 <Link to='/chat' className='dropdown-item'>Chat</Link>
                 <Link to={`/profile/${props.username}`} className='dropdown-item'>Profile</Link>
                 <button onClick={e => logOut()} className='dropdown-item'>Log Out</button>
             </DropdownButton>
-            <img src="/Users/surbhi/Clone/seniorproject/src/smollogo.png" align="left"/>
+            <img src={smallLogo} height="50" width="auto"/>
         </div>
     );
 }
