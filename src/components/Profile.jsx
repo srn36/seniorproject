49a21f9563725e8939076a6ef401e63bf92418b5
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-10-18 20:25:37 -0400

    attempt at mock friend list, using logo as pfp

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { fetchFriendsForUser } from "../helper/apiCalls";
+import { getToken } from "../helper/tokens";
+import { Table } from "react-bootstrap";
 
 function Profile(props) {
-    
-    return (
-        <div>
-            <h4>Profile Page</h4>
-        </div>
-    );
+    const token = getToken();
+
+    const friendList = /*async*/ (t) => {
+        return /*await*/ fetchFriendsForUser(t)/*.then(results => results.json())*/;
+    };
+
+    const friends = friendList(token);
+    const content = useMemo(() => {
+        const friendTable = Array.isArray(friends) 
+                            && friends.map(friend => (
+                                <tr key={friend.username}>
+                                    <td key={friend.username}>{friend.username}</td>
+                                </tr>
+                            ));
+        return (
+            <div>
+                <h2>Profile Page</h2>
+                <div className="row">
+                    <h4>hi</h4>
+                </div>
+                <div className="row" style={{placeItems: 'center', display: 'flex', flexDirection: 'column'}}>
+                    <h3>Friend List</h3>
+                    <div className="col-6" style={{display: 'flex'}}>
+                        {
+                            (Object.keys(friendTable).length > 0) &&
+                            <Table striped bordered title="Friend List">
+                                <thead>
+                                    <tr>
+                                        <th>Username</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {friendTable}
+                                </tbody>
+                            </Table>
+                        }
+                    </div>
+                </div>
+            </div>
+        );
+    }, [friends]);
+
+    return content;
 }
 
 export default Profile;
--- src/helper/apiCalls.js
+++ src/helper/apiCalls.js
@@ -1,3 +1,5 @@
+import * as ProfilePic from '../logo192.png';
+
 export /*async*/ function fetchLoginTokenFromCredentials(credentials) {
     /*
     return fetch('login token service', {
@@ -26,6 +28,33 @@ export /*async*/ function fetchUserInfoFromToken(token) {
     );
     */
     return {username: 'username', password: 'password'};
+}
+
+export /*async*/ function fetchFriendsForUser(userToken) {
+    /*
+    return fetch('token? to friends list service', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({'token': token})
+    }).then(data => 
+        data.json()
+    );
+    */
+    const mockFriendList = [
+        {id: 'Friend 1', username: 'Friend 1', profilePicture: <img src={ProfilePic} alt='Profile Pic'/>},
+        {id: 'Friend 2', username: 'Friend 2', profilePicture: <img src={ProfilePic} alt='Profile Pic'/>},
+        {id: 'Friend 3', username: 'Friend 3', profilePicture: <img src={ProfilePic} alt='Profile Pic'/>},
+        {id: 'Friend 4', username: 'Friend 4', profilePicture: <img src={ProfilePic} alt='Profile Pic'/>},
+        {id: 'Friend 5', username: 'Friend 5', profilePicture: <img src={ProfilePic} alt='Profile Pic'/>},
+        {id: 'Friend 6', username: 'Friend 6', profilePicture: <img src={ProfilePic} alt='Profile Pic'/>},
+        {id: 'Friend 7', username: 'Friend 7', profilePicture: <img src={ProfilePic} alt='Profile Pic'/>},
+        {id: 'Friend 8', username: 'Friend 8', profilePicture: <img src={ProfilePic} alt='Profile Pic'/>},
+        {id: 'Friend 9', username: 'Friend 9', profilePicture: <img src={ProfilePic} alt='Profile Pic'/>},
+        {id: 'Friend 10', username: 'Friend 10', profilePicture: <img src={ProfilePic} alt='Profile Pic'/>}
+    ];
+    return mockFriendList;
 }
 
 export async function fetchFeedForUser(userToken, pageParam) {
