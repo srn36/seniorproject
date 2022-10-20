83dbe094d3b5c90896c711b3044ea9cd3e0ac9d4
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-10-19 20:27:30 -0400

    omg the header stuff social distances

--- src/components/AuthRoute.jsx
+++ src/components/AuthRoute.jsx
@@ -3,6 +3,7 @@ import { getToken } from "../helper/tokens";
 import { fetchUserInfoFromToken } from "../helper/apiCalls";
 import { Navigate } from "react-router-dom/dist";
 import NavigationBar from "./NavigationBar";
+import smallLogo from '../smollogo.png';
 
 function AuthRoute({ child }) {
     const token = getToken();
@@ -15,6 +16,9 @@ function AuthRoute({ child }) {
         const pageContent = (
             <div className="App">
                 <header className='App-header'>
+                    <div style={{display: 'flex'}}>
+                        <img src={smallLogo} height="50" width="auto" alt="logo"/>
+                    </div>
                     <NavigationBar setContent={setContent} username={username}/>
                 </header>
                 <main>
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -8,23 +8,20 @@ import { Navigate } from "react-router-dom/dist";
 import { DropdownButton } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import smallLogo from '../smollogo.png';
 
 function NavigationBar(props) {
     const logOut = () => {
         sessionStorage.removeItem('token');
         return props.setContent(<Navigate to='/login/' />);
     };
-//style={{display: 'flex'}}
     return (
-        <div>
+        <div style={{display: 'flex'}}>
             <DropdownButton id="dropdown-basic-button" title="Navigation">
                 <Link to='/' className='dropdown-item'>Home</Link>
                 <Link to='/chat' className='dropdown-item'>Chat</Link>
                 <Link to={`/profile/${props.username}`} className='dropdown-item'>Profile</Link>
                 <button onClick={e => logOut()} className='dropdown-item'>Log Out</button>
             </DropdownButton>
-            <img src={smallLogo} height="50" width="auto" alt="logo"/>
         </div>
     );
 }
