170b0b6d81f74758015c2d1ef1b33168c230ee7d
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-10-23 13:58:06 -0400

    search bar to filter friends list results

--- src/components/friend-displays/FriendList.jsx
+++ src/components/friend-displays/FriendList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import PropTypes from 'prop-types';
 import FriendRow from "./FriendRow";
 import RemovableFriendRow from "./RemovableFriendRow";
@@ -7,7 +7,6 @@ import { Table } from "react-bootstrap";
 
 function FriendList(props) {
     const {friends, type, userInfo} = props;
-
     const friendTable = friends.map(friend => {
         const rowTypes = {
             Standard: <FriendRow key={friend.username} username={friend.username} profilePic={friend.profilePic} userInfo={userInfo}/>,
@@ -16,18 +15,40 @@ function FriendList(props) {
         };
         return rowTypes[type];
     });
-    return (
-        <div className="friend-list">
-            {
-                (Object.keys(friendTable).length > 0) &&
+
+    const [filteredTable, setFilteredTable] = useState(friendTable);
+
+    return useMemo(() => {
+        const filter = (text) => {
+            return friends.filter(friend => friend.username.includes(text)).map(friend => {
+                const rowTypes = {
+                    Standard: <FriendRow key={friend.username} username={friend.username} profilePic={friend.profilePic} userInfo={userInfo}/>,
+                    Removable: <RemovableFriendRow key={friend.username} username={friend.username} profilePic={friend.profilePic} userInfo={userInfo}/>,
+                    Requests: <FriendRequestRow key={friend.fromUsername} username={friend.fromUsername} profilePic={friend.profilePic} userInfo={userInfo}/>
+                };
+                return rowTypes[type];
+            });
+        }
+        return (
+            <div className="friend-list">
                 <Table bordered hover>
+                    <thead>
+                        <tr>
+                            <th style={{display: 'flex', flexDirection: 'row'}}>
+                                <p style={{marginBottom: '0px', marginRight: '0.5em'}}>Filter List</p>
+                                <input type="text" onChange={e => setFilteredTable(filter(e.target.value))}/>
+                            </th>
+                        </tr>
+                    </thead>
                     <tbody>
-                        {friendTable}
+                        {
+                            (Object.keys(filteredTable).length > 0) ? filteredTable : <tr><td><p>No Results</p></td></tr>
+                        }
                     </tbody>
                 </Table>
-            }
-        </div>
-    );
+            </div>
+        );
+    }, [friends, type, userInfo, filteredTable, setFilteredTable]);
 }
 
 FriendList.propTypes = {
