2700fa974b393cbf3529c4d784aeac782bf8e227
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-10-24 16:00:30 -0400

    divider separating logout from other tabs

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,7 +3,7 @@ Use of this component has now been packaged into AuthRoute, which will serve as 
 Load pages with AuthRoute to automatically have a NavigationBar appended
 */
 import React from "react";
-import { DropdownButton } from 'react-bootstrap';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
@@ -21,6 +21,7 @@ function NavigationBar(props) {
             <Link to='/chat' className='dropdown-item' state={{userInfo: userInfo}}>Chat</Link>
             <Link reloadDocument to={`/profile/${userInfo?.username}`} className='dropdown-item' state={{userInfo: userInfo}}>Profile</Link>
             <Link to='/requests/' className='dropdown-item' state={{userInfo: userInfo}}>Friend Requests</Link>
+            <Dropdown.Divider />
             <button onClick={_e => logOut()} className='dropdown-item'>Log Out</button>
         </DropdownButton>
     );
