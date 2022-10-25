8c30e5f578e029e1601d386331bb20aae7f6b8a2
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-10-24 22:55:26 -0400

    IT ACTUALLY CLOSES

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,13 +2,14 @@
 Use of this component has now been packaged into AuthRoute, which will serve as a base page.
 Load pages with AuthRoute to automatically have a NavigationBar appended
 */
-import React from "react";
+import React, { useRef } from "react";
 import { Dropdown, DropdownButton } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function NavigationBar(props) {
     const userInfo = props.userInfo;
+    const closeDropdownRef = useRef(null);
 
     const logOut = () => {
         localStorage.removeItem('token');
@@ -16,11 +17,39 @@ function NavigationBar(props) {
     };
 
     return (
-        <DropdownButton id="dropdown-basic-button" title="Navigation">
-            <Link to='/' className='dropdown-item' state={{userInfo: userInfo}}>Home</Link>
-            <Link to='/chat' className='dropdown-item' state={{userInfo: userInfo}}>Chat</Link>
-            <Link reloadDocument to={`/profile/${userInfo?.username}`} className='dropdown-item' state={{userInfo: userInfo}}>Profile</Link>
-            <Link to='/requests/' className='dropdown-item' state={{userInfo: userInfo}}>Friend Requests</Link>
+        <DropdownButton id="dropdown-basic-button" title="Navigation" ref={closeDropdownRef}>
+            <Link
+                to='/'
+                className='dropdown-item'
+                onClick={_e => closeDropdownRef.current.click()}
+                state={{userInfo: userInfo}}
+            >
+                Home
+            </Link>
+            <Link
+                to='/chat'
+                className='dropdown-item'
+                onClick={_e => closeDropdownRef.current.click()}
+                state={{userInfo: userInfo}}
+            >
+                Chat
+            </Link>
+            <Link reloadDocument
+                to={`/profile/${userInfo?.username}`}
+                className='dropdown-item'
+                onClick={_e => closeDropdownRef.current.click()}
+                state={{userInfo: userInfo}}
+            >
+                Profile
+            </Link>
+            <Link
+                to='/requests/'
+                className='dropdown-item'
+                onClick={_e => closeDropdownRef.current.click()}
+                state={{userInfo: userInfo}}
+            >
+                Friend Requests
+            </Link>
             <Dropdown.Divider />
             <button onClick={_e => logOut()} className='dropdown-item'>Log Out</button>
         </DropdownButton>
