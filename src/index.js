99d9c86f7783adfe30c8d3e8dde99e072afd87a9
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-10-25 19:03:07 -0400

    more organized css

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import './styles/index.css';
 import './styles/App.css';
 import './styles/Login.css';
 import './styles/Friend.css';
+import './styles/Posts.css';
+import './styles/Profile.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const queryClient = new QueryClient();
