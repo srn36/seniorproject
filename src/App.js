9e8e39b267e2e6e25ee179ac96a7f27fc34c7139
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-11-03 10:38:51 -0400

    basic login functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import routes from './helper/routes';
+import bg from './BG.jpeg';
 import { RouterProvider } from 'react-router-dom';
+import { withAuthenticator, View } from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut }) {
     const router = routes();
-    return <RouterProvider router={router} />;
+    return (
+        <View>
+            <main>
+                <img className="background-pic" src={bg} alt="" />
+            </main>
+            <div className='App'>
+                <button onClick={signOut}>sign out</button>
+                <RouterProvider router={router} />
+            </div>
+        </View>
+    );
 }
 
-export default App;
+export default withAuthenticator(App);
