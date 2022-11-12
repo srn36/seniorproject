164bf9a5dbe159b22613f4108a6751c0a1c7acea
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-11-12 05:38:47 -0500

    added dividers to make game list look fancy

--- src/components/game-checkboxes/GameCheckboxes.jsx
+++ src/components/game-checkboxes/GameCheckboxes.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { CheckboxField, Flex } from '@aws-amplify/ui-react';
 import { games } from '../../helper/game-selection';
 import CheckboxLabel from './CheckboxLabel';
+import { Dropdown } from 'react-bootstrap';
 
 function GameCheckboxes({ validationErrors }) {
     const allFalseCheckValues = games.map(_game => false);
@@ -19,42 +20,47 @@ function GameCheckboxes({ validationErrors }) {
         const gameTitle = games[index].title;
         return (
             // This check ensures that the error message is only displayed once instead of after each checkbox
-            parseInt(index) === (gamesKeys.length - 1) ?
-                <CheckboxField
-                    errorMessage={validationErrors.game || validationErrors[gameTitle]}
-                    hasError={!!validationErrors.game || !!validationErrors[gameTitle]}
-                    key={index}
-                    name={gameTitle}
-                    label={
-                        <CheckboxLabel {
-                            ...{
-                                'disable': !selectedGames[index],
-                                'hasError': !!validationErrors[gameTitle],
-                                ...games[index]
-                            }
-                        }/>
-                    }
-                    value='game-selected'
-                    onChange={e => handleGameChecked(index, e)}
-                />
+            parseInt(index) === (gamesKeys.length - 1) ?               
+                <React.Fragment key={index}>
+                    <Dropdown.Divider/>
+                    <CheckboxField
+                        errorMessage={validationErrors.game || validationErrors[gameTitle]}
+                        hasError={!!validationErrors.game || !!validationErrors[gameTitle]}
+                        name={gameTitle}
+                        label={
+                            <CheckboxLabel {
+                                ...{
+                                    'disable': !selectedGames[index],
+                                    'hasError': !!validationErrors[gameTitle],
+                                    ...games[index]
+                                }
+                            }/>
+                        }
+                        value='game-selected'
+                        onChange={e => handleGameChecked(index, e)}
+                    />
+                </React.Fragment>
                 :
-                <CheckboxField
-                    errorMessage={validationErrors[gameTitle]}
-                    hasError={!!validationErrors.game || !!validationErrors[gameTitle]}
-                    key={index}
-                    name={gameTitle}
-                    label={
-                        <CheckboxLabel {
-                            ...{
-                                'disable': !selectedGames[index],
-                                'hasError': !!validationErrors[gameTitle],
-                                ...games[index]
-                            }
-                        }/>
-                    }
-                    value='game-selected'
-                    onChange={e => handleGameChecked(index, e)}
-                />
+                <React.Fragment key={index}>
+                    <Dropdown.Divider/>
+                    <CheckboxField
+                        errorMessage={validationErrors[gameTitle]}
+                        hasError={!!validationErrors.game || !!validationErrors[gameTitle]}
+                        name={gameTitle}
+                        label={                        
+                            <CheckboxLabel {
+                                ...{
+                                    'disable': !selectedGames[index],
+                                    'hasError': !!validationErrors[gameTitle],
+                                    ...games[index]
+                                }
+                            }/>
+                        }
+                        value='game-selected'
+                        onChange={e => handleGameChecked(index, e)}
+                    />                
+                </React.Fragment>
+                
         );
     });
 
