c423baeddcaea79220452d67df296d1412e1998b
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-11-12 20:06:52 -0500

    actually use site username when checkbox is selected

--- src/App.js
+++ src/App.js
@@ -34,17 +34,34 @@ function App() {
                 },
             }}
             services={{
-                /* 
-                This what decides if the "sign up" button at the bottom of the form is enabled or disabled.
-                Assigning a validationError to a CheckBox makes it required -- I haven't tested text input fields, but I imagine it'll be similar.
-                */
+                /** 
+                 * This what decides if the "sign up" button at the bottom of the form is enabled or disabled.
+                 * 
+                 * Assigning a validationError to a CheckBox makes it required --
+                 * I haven't tested text input fields, but I imagine it'll be similar.
+                 */
                 async validateCustomSignUp(formData) {
-                    // Separate arrays containing each selected game and console
+                    const username = formData.username;
+
+                    // Create separate arrays containing each selected game and console
                     const selectedGames = Object.keys(formData).filter(dataKey => formData[dataKey] === 'game-selected');
                     const selectedConsoles = Object.keys(formData).filter(dataKey => formData[dataKey] === 'console-selected');
 
                     const gameToInfoMap = {};
                     const validateErrors = selectedGames.length > 0 ? {} : {game: 'You must select at least one game'};
+
+                    /**
+                     * Iterate over every selected game and 
+                     * attempt to match it to at least one selected console.
+                     * 
+                     * If a match is found, convert that console selection to 
+                     * a string that only contains the name of the console.
+                     * 
+                     * Next, check to make sure that there is a username associated with the game.
+                     * 
+                     * Then, package the console selections and the username into a list item
+                     * and store that list item in gameToInfoMap with the game's title as a key.
+                     */
                     selectedGames.forEach(gameTitle => {
                         // I can explain, I swear
                         const selectedConsolesForTitle = selectedConsoles.filter(gameConsoleLabel => 
@@ -56,9 +73,10 @@ function App() {
                         // Store the consoles and username listed for each game
                         gameToInfoMap[gameTitle] = {
                             'consoles': [...selectedConsolesForTitle],
-                            'username': !!formData[`${gameTitle}-Default-Username`] ? 1 : (formData[`${gameTitle}-Username`] ||  '')
+                            'username': !!formData[`${gameTitle}-Default-Username`] ? username : (formData[`${gameTitle}-Username`] ||  '')
                         };
 
+                        // Add validation errors
                         if(!formData[`${gameTitle}-Username`] && !formData[`${gameTitle}-Default-Username`]) {
                             validateErrors[`${gameTitle}-Username`] = `Input username for ${gameTitle}`;
                         }
@@ -66,7 +84,7 @@ function App() {
                             validateErrors[gameTitle] = `You must select at least one console for ${gameTitle}`;
                         }
                     });
-                    
+
                     // Check if there is at least 1 validation error
                     if (Object.keys(validateErrors).length > 0) {
                         return validateErrors;
