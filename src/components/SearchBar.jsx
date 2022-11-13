5897a235e32a5e74315e789367ec955aca5233a8
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-11-13 18:49:47 -0500

    Removal of search button and prep for results

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,35 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import { TextField } from '@aws-amplify/ui-react';
+import mockPFP from './../logo192.png';
 
+// Mock data for testing
+const createMockUserList = () => {
+    let userList = [
+        {key: 'Alejandro Escamilla', username: 'Alejandro Escamilla', profilePic: mockPFP},
+        {key: 'Paul Jarvis', username: 'Paul Jarvis', profilePic: mockPFP},
+        {key: 'test1', username: 'test1', profilePic: mockPFP},
+        {key: 'Aleks Dorohovich', username: 'Aleks Dorohovich', profilePic: mockPFP},
+    ];
+    for(let i = 0; i < 15; i++) {
+        userList = [{key: `User ${i}`, username: `User ${i}`, profilePic: mockPFP}, {key: `Person ${i}`, username: `Person ${i}`, profilePic: mockPFP}, ...userList];
+    }
+    return userList;
+};
+const mockUserList = createMockUserList();
+const sendMockQuery = (query) => {
+    return mockUserList.filter(user => user.username.includes(query));
+};
 
 function SearchBar(props) {
+    const MIN_CHARS_FOR_QUERY = 3;
     const [queryText, setQueryText] = useState('');
     const [results, setResults] = useState([]);
-    const [suggestions, setSuggestions] = useState([]);
 
     useEffect(() => {
-        const sendQuery = (query) => [];
-        if(queryText.length > 0) {
-            const queryResults = sendQuery(queryText);
+        if(queryText.length >= MIN_CHARS_FOR_QUERY) {
+            const queryResults = sendMockQuery(queryText);
             setResults(queryResults);
+        }
+        else {
+            setResults([]);
         }
     }, [queryText, setResults]);
 
-    useEffect(() => {
-
-    }, [results, setSuggestions]);
+    console.log(results);
 
     return (
-        <div className='search-bar'>
-            <div className={`bar-and-results-${!!suggestions.length}`}>
-                <TextField
-                    label='Search Bar'
-                    placeholder='Search for users'
-                    onChange={e => setQueryText(e.target.value)}
-                    labelHidden
-                />
-            </div>
-            <button>S</button>
+        <div className={`search-bar-${!!results.length}`}>
+            <TextField
+                label='Search Bar'
+                placeholder='Search for users'
+                onChange={e => setQueryText(e.target.value)}
+                labelHidden
+            />
         </div>
     );
 }
