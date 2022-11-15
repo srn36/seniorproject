fdc021950cb352ca363319116e0c35151914be0e
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-11-15 11:38:50 -0500

    honestly more readable without comments

--- src/App.js
+++ src/App.js
@@ -13,16 +13,11 @@ function App() {
 
     return (
         <Authenticator
-            // Default to Sign In screen
             initialState="signIn"
-            
-            // Customize Authenticator.SignUp.FormFields
             components={{
                 SignUp: {
                     FormFields() {
-                        // (context) => [context.validationErrors] ensures reloads only happen when validationErrors changes
                         const { validationErrors } = useAuthenticator((context) => [context.validationErrors]);
-            
                         return (
                             <>
                                 <Authenticator.SignUp.FormFields />                                
@@ -33,34 +28,13 @@ function App() {
                 },
             }}
             services={{
-                /** 
-                 * This what decides if the "sign up" button at the bottom of the form is enabled or disabled.
-                 * 
-                 * Assigning a validationError to a CheckBox makes it required --
-                 * I haven't tested text input fields, but I imagine it'll be similar.
-                 */
                 async validateCustomSignUp(formData) {
                     const username = formData.username;
-
-                    // Create separate arrays containing each selected game and console
                     const selectedGames = Object.keys(formData).filter(dataKey => formData[dataKey] === 'game-selected');
-                    const selectedConsoles = Object.keys(formData).filter(dataKey => formData[dataKey] === 'console-selected');
-
+                    const selectedConsoles = Object.keys(formData).filter(dataKey => formData[dataKey] === 'console-selected')
                     const gameToInfoMap = {};
                     const validateErrors = selectedGames.length > 0 ? {} : {game: 'You must select at least one game'};
 
-                    /**
-                     * Iterate over every selected game and 
-                     * attempt to match it to at least one selected console.
-                     * 
-                     * If a match is found, convert that console selection to 
-                     * a string that only contains the name of the console.
-                     * 
-                     * Next, check to make sure that there is a username associated with the game.
-                     * 
-                     * Then, package the console selections and the username into a list item
-                     * and store that list item in gameToInfoMap with the game's title as a key.
-                     */
                     selectedGames.forEach(gameTitle => {
                         // I can explain, I swear
                         const selectedConsolesForTitle = selectedConsoles.filter(gameConsoleLabel => 
@@ -69,7 +43,6 @@ function App() {
                             // selectedConsoles has strings formatted as '<gameTitle>-<console>', we want '<console>'
                             selectedConsole.substring(gameTitle.length + 1)
                         );
-                        // Store the consoles and username listed for each game
                         gameToInfoMap[gameTitle] = {
                             'consoles': [...selectedConsolesForTitle],
                             /**
@@ -90,14 +63,12 @@ function App() {
                         }
                     });
 
-                    // Check if there is at least 1 validation error
                     if (Object.keys(validateErrors).length > 0) {
                         return validateErrors;
                     }
                 },
             }}
         >
-            {/* This stuff down here is simply rendering the app itself */}
             {({ signOut, user }) => (
                 <View>
                     <main>
