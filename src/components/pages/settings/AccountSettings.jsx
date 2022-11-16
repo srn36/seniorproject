3a34200f9d87cb1bf2ca88273a8f490a662e6b67
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-11-16 17:44:33 -0500

    this is incorrect but i cant fix it

--- src/components/pages/settings/AccountSettings.jsx
+++ src/components/pages/settings/AccountSettings.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Auth } from 'aws-amplify';
+import { Divider, TextField } from '@aws-amplify/ui-react';
 
 function AccountSettings(props) {
     /**
@@ -14,9 +16,47 @@ function AccountSettings(props) {
      * 
      */
 
+    const [oldPassword, setOldPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmNewPassword, setConfirmNewPassword] = useState('');
+
+    const changePassword = (oldPassword, newPassword, confirmNewPassword) => {
+        if(newPassword !== confirmNewPassword) {
+            window.alert('Passwwords must match');
+        }
+        
+        Auth.currentAuthenticatedUser().then(user => {
+            return Auth.changePassword(user, oldPassword, newPassword);
+        }).then(data => 
+            console.log(data)
+        ).catch(err => 
+            console.log(err)
+        );
+    };
+    
     return (
         <div>
             Account Settings
+            <Divider/>
+            Change Password
+            <form onSubmit={changePassword(oldPassword, newPassword, confirmNewPassword)}>
+                <TextField
+                    placeholder="Old Password"
+                    label="Old Password"
+                    onChange={e => setOldPassword(e.target.value)}
+                />
+                <TextField
+                    placeholder="New Password"
+                    label="New Password"
+                    onChange={e => setNewPassword(e.target.value)}
+                />
+                <TextField
+                    placeholder="Confirm New Password"
+                    label="Confirm New Password"
+                    onChange={e => setConfirmNewPassword(e.target.value)}
+                />
+                <button type='submit'>Change Password</button>
+            </form>
         </div>
     );
 
