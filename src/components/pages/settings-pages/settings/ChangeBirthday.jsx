1e072015c143b2887ffc702d52f44f8e5be6f32d
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-11-19 18:28:56 -0500

    proper birthday input - partial validation

--- src/components/pages/settings-pages/settings/ChangeBirthday.jsx
+++ src/components/pages/settings-pages/settings/ChangeBirthday.jsx
@@ -1,32 +1,61 @@
 import React, { useState } from 'react';
-import { TextAreaField } from '@aws-amplify/ui-react';
+import { TextField } from '@aws-amplify/ui-react';
 import SettingsForm from './SettingsForm';
 
 function ChangeBirthday(props) {
-    const initialBirthday = 'Birthday' /* fetch birthday somehow */;
-    const [birthday, setBirthday] = useState(initialBirthday);
+    const [birthday, setBirthday] = useState('');
 
-    const formFields = [
-        <TextAreaField
+    const birthdayInput = (
+        <TextField
             key='Birthday'
             label='Edit Birthday'
-            placeholder='Input Birthday'
-            defaultValue={initialBirthday}
+            placeholder='MM/DD/YYYY'
             onChange={(e) => setBirthday(e.target.value)}
         />
+    )
+
+    const formFields = [
+        birthdayInput
     ];
 
     const changeBirthday = () => {
         window.alert('Not Implemented');
     };
+
+    const validateBirthday = () => {
+        const avoidEmojis = [...birthday];
+        if(!(avoidEmojis[2] === '/' && avoidEmojis[5] === '/')) {
+            return false;
+        }
+
+        // Month validation
+        const month = parseInt(birthday.substring(0,2));
+        if(isNaN(month) || month < 1 || month > 12) {
+            return false;
+        }
+
+        // Year validation
+        const year = parseInt(birthday.substring(6));
+        if(isNaN(year) || year < 1900 || year > 2022) { // Fix upper limit to dynamic current year
+            return false;
+        }
+
+        // Date validation
+        const day = parseInt(birthday.substring(3,5));
+        if(isNaN(day) || day < 1 || day > 31) { // Fix date validation
+            return false;
+        }
+
+        return true;
+    }
     
     return (
         <SettingsForm
             title='Change Birthday'
             fields={formFields}
             onSubmit={changeBirthday}
-            submitLabel='Change Birthday'
-            submitDisabled={birthday === initialBirthday}
+            submitLabel='Update Birthday'
+            submitDisabled={!validateBirthday()}
         />
     );
 }
