a3fd27ec89eef70ede74e6b8b43ce34700772b0c
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-11-22 16:51:37 -0500

    chat page go brrrrrrrrr

--- src/components/pages/Chat.jsx
+++ src/components/pages/Chat.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 
 function Chat(props) {
+    const requestBody = JSON.stringify({
+        'Filter': '',
+        'Limit': 1000,
+        'UserPoolId': 'us-east-1_gameon',
+    });
+    
+    const users = fetch('https://cognito-idp.us-east-1.amazonaws.com/', {
+        method: 'POST',
+        headers: {
+            'x-amz-target': 'AWSCognitoIdentityProviderService.ListUsers',
+            'Content-Type': 'application/x-amz-json-1.1',
+        },
+        body: requestBody,
+    }).then(response => console.log(response)).then(response => response.json());
+    console.log(users);
     
     return (
         <h4>Chat Page</h4>
