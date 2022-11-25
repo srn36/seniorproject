ed03ee69d1bff6a80dac5a2bb0bb7a53f3cf9597
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-11-25 15:42:51 -0500

    i hate this

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,15 @@
 export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "gameon": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
     "storage": {
         "s3gameonpfpstorage00856971": {
             "BucketName": "string",
