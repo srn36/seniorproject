90d22645f06fddd7a5d13d21a4dc9622e261a393
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-11-25 14:28:14 -0500

    cognito sucks

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "gameon": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
     "storage": {
         "s3gameonpfpstorage00856971": {
             "BucketName": "string",
