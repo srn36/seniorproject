e37fc96d65434617debca68a6c03f394e118b51c
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-11-26 20:47:39 -0500

    Untested date validation

--- src/components/pages/settings-pages/settings/ChangeBirthday.jsx
+++ src/components/pages/settings-pages/settings/ChangeBirthday.jsx
@@ -4,6 +4,16 @@ import {
 } from '../../../../ui-components';
 import { Divider } from '@aws-amplify/ui-react';
 
+function maxDaysByMonth(month, year) {
+    if(month === 2) {
+        return (year % 4 === 0) ? 29 : 28;
+    } else if(month === 4 || month === 6 || month === 9 || month === 11) {
+        return 30;
+    } else {
+        return 31;
+    }
+}
+
 function ChangeBirthday(props) {
     const [birthday, setBirthday] = useState('');
 
@@ -17,21 +27,37 @@ function ChangeBirthday(props) {
             return false;
         }
 
-        // Month validation
+        const currentMonth = currentTime.getMonth() + 1;
+        const currentDay = currentTime.getDate();
+        const currentYear = currentTime.getFullYear();
+
         const month = parseInt(birthday.substring(6,8));
-        if(isNaN(month) || month < 1 || month > 12) {
+        const day = parseInt(birthday.substring(9));
+        const year = parseInt(birthday.substring(0,4));
+
+        // Check numeric
+        if(isNaN(month) || isNaN(day) || isNaN(year)) {
             return false;
         }
 
-        // Year validation
-        const year = parseInt(birthday.substring(0,4));
-        if(isNaN(year) || year < 1900 || year > 2022) { // Fix upper limit to dynamic current year
+        const maxDays = maxDaysByMonth(month, year);
+
+        // Month validation
+        if(month < 1 || month > 12) {
             return false;
         }
 
         // Date validation
-        const day = parseInt(birthday.substring(9));
-        if(isNaN(day) || day < 1 || day > 31) { // Fix date validation
+        if(day < 1 || day > maxDays) { // Fix date validation
+            return false;
+        }
+
+        // Year validation
+        if(year < currentYear - 110 || year > currentYear - 13) {
+            // Check if the user turned 13 this year
+            if(year === currentYear - 13 && month <= currentMonth && day <= currentDay) {
+                return true;
+            }
             return false;
         }
 
