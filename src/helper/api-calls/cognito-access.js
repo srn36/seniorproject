11929fbf739a0533900580691517da3ad3a14c8a
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-11-30 17:28:38 -0500

    getUser function

--- src/helper/api-calls/cognito-access.js
+++ src/helper/api-calls/cognito-access.js
@@ -14,6 +14,23 @@ export async function searchUsers(filterText = '') {
         }
     }
     const { ...rest } = await API.get(apiName, path, myInit);
+    return rest;
+}
+
+export async function getUser(username) {
+    const apiName = 'AdminQueries';
+    const path = '/getUser';
+    const myInit = {
+        queryStringParameters: {
+            Username: `${username}`,
+            UserPoolId: 'us-east-1_gameon'
+        },
+        headers: {
+          'Content-Type' : 'application/x-amz-json-1.1',
+          Authorization: `${(await Auth.currentSession()).getAccessToken().getJwtToken()}`
+        }
+    }
+    const { ...rest } = await API.get(apiName, path, myInit);
     console.log(rest);
     return rest;
 }
