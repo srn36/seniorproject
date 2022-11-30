3b36e4db344604502b995b021def83d269710662
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-11-30 18:22:48 -0500

    error page no longer a snitch

--- src/components/pages/ErrorPage.jsx
+++ src/components/pages/ErrorPage.jsx
@@ -1,22 +1,7 @@
 import React from 'react';
 
 function ErrorPage(props) {
-    return (
-        <>
-            <h1>It seems the page you're looking for doesn't exist...</h1>
-            <h3>
-                If you believe this is an error, please report the issue {
-                    <a 
-                        href='https://github.com/srn36/seniorproject/issues'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        here
-                    </a>
-                }                
-            </h3>
-        </>
-    );
+    return <h1>It seems the page you're looking for doesn't exist...</h1>;
 }
 
 export default ErrorPage;
