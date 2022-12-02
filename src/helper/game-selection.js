0e350256ea3ca55c6973aba4cf24bb8645d8cc94
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-12-02 17:21:35 -0500

    reduced number of mock games

--- src/helper/game-selection.js
+++ src/helper/game-selection.js
@@ -4,18 +4,6 @@ export const games = [
     {icon: placeholderImg, title: 'Game 00', consoleOptionString: '111'},
     {icon: placeholderImg, title: 'Game 01', consoleOptionString: '111'},
     {icon: placeholderImg, title: 'Game 02', consoleOptionString: '111'},
-    {icon: placeholderImg, title: 'Game 03', consoleOptionString: '111'},
-    {icon: placeholderImg, title: 'Game 04', consoleOptionString: '111'},
-    {icon: placeholderImg, title: 'Game 05', consoleOptionString: '111'},
-    {icon: placeholderImg, title: 'Game 06', consoleOptionString: '111'},
-    {icon: placeholderImg, title: 'Game 07', consoleOptionString: '111'},
-    {icon: placeholderImg, title: 'Game 08', consoleOptionString: '111'},
-    {icon: placeholderImg, title: 'Game 09', consoleOptionString: '111'},
-    {icon: placeholderImg, title: 'Game 10', consoleOptionString: '111'},
-    {icon: placeholderImg, title: 'Game 11', consoleOptionString: '111'},
-    {icon: placeholderImg, title: 'Game 12', consoleOptionString: '111'},
-    {icon: placeholderImg, title: 'Game 13', consoleOptionString: '111'},
-    {icon: placeholderImg, title: 'Game 14', consoleOptionString: '111'},
 ];
 
 export const consoles = [
