9cc11e2acbc0fb07d7184c066d7e6a42944f7faa
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-12-02 20:42:47 -0500

    remove profile pic from S3 on account delete

--- src/components/pages/settings-pages/settings/DeleteAccount.jsx
+++ src/components/pages/settings-pages/settings/DeleteAccount.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Auth } from 'aws-amplify';
+import { Auth, Storage } from 'aws-amplify';
 import SettingsForm from './SettingsForm';
 import { useNavigate } from 'react-router-dom';
 
-function DeleteAccount(props) {
+function DeleteAccount({ username }) {
     const navigate = useNavigate();
     const formFields = [];
     
-    const deleteAccount = () => {
+    const deleteAccount = async () => {
         if(window.confirm('Delete your account?')) {
+            await Storage.remove(`${username}-profilepic`);
             Auth.deleteUser();
             navigate('/');
         }
