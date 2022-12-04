b62c2dbc5a46c9ec164607681da8a64c243c9cbb
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-12-04 17:50:48 -0500

    email verification does not close prompt when you switch tabs

--- src/components/pages/settings-pages/settings/ChangeEmail.jsx
+++ src/components/pages/settings-pages/settings/ChangeEmail.jsx
@@ -5,6 +5,7 @@ import SettingsForm from './SettingsForm';
 
 function ChangeEmail({ email }) {
     const [newEmail, setNewEmail] = useState('');
+    const [code, setCode] = useState('');
 
     const formFields = [
         <TextField
@@ -21,11 +22,48 @@ function ChangeEmail({ email }) {
         />
     ];
 
+    const verifyEmail = async () => new Promise((resolve) => {
+        const dialog = document.getElementById('confirm');
+        const verifyButton = document.getElementById('verify-button');
+
+        // I don't want the user to be able to exit the modal without inputting a 6 number code
+        dialog.addEventListener('keydown', (e) => {
+            if(e.key === 'Escape') {
+                e.preventDefault();
+            }
+        });
+        dialog.addEventListener('keypress', (e) => {
+            if(e.key === 'Escape') {
+                e.preventDefault();
+            }
+        });
+
+        // Listen for when the form is submitted
+        dialog.addEventListener('close', () => {
+            return resolve(dialog.returnValue);
+        });
+
+        /**
+         * Apparently Chrome for Android and a few minor browsers do not support the 'close'
+         * event for dialog components?!?!?! This technique for detecting close events works
+         * for all browsers
+         */ 
+        verifyButton.addEventListener('click', () => {
+            dialog.close();
+            return resolve(dialog.returnValue);
+        });
+
+        dialog.showModal();
+    });
+
     const changeEmail = async () => {
         const user = await Auth.currentAuthenticatedUser();
         const updateAttributes = {email: newEmail};
-        Auth.updateUserAttributes(user, updateAttributes).then(async () =>
-            await Auth.verifyCurrentUserAttributeSubmit('email', window.prompt('Enter the confirmation code:'))
+        Auth.updateUserAttributes(user, updateAttributes).then(async () => {
+            const val = await verifyEmail();
+            return val;
+        }).then(async codeVal =>
+            await Auth.verifyCurrentUserAttributeSubmit('email', codeVal)
         ).then(() => {
             window.alert('Email updated successfully');
             window.location.reload();
@@ -33,13 +71,32 @@ function ChangeEmail({ email }) {
     };
 
     return (
-        <SettingsForm
-            title='Change Email'
-            fields={formFields}
-            onSubmit={changeEmail}
-            submitLabel='Change Email'
-            submitDisabled={!(newEmail.length > 0 && newEmail !== email)}
-        />
+        <>
+            <SettingsForm
+                title='Change Email'
+                fields={formFields}
+                onSubmit={changeEmail}
+                submitLabel='Change Email'
+                submitDisabled={!(newEmail.length > 0 && newEmail !== email)}
+            />
+            <dialog id='confirm'>
+                <p>A verification code has been sent to your email address</p>
+                <form method='dialog'>
+                    <TextField
+                        placeholder='Enter verification code'
+                        onChange={e => setCode(e.target.value)}
+                    />
+                    <button 
+                        id='verify-button'
+                        value={code} 
+                        disabled={code.length !== 6}
+                    >
+                        Verify
+                    </button>
+                </form>
+            </dialog>
+        </>
+        
     );
 }
 
