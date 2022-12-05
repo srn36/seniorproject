5290abd3d85deaa0043b08c7b39e885bfcbe120d
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-12-05 03:33:59 -0500

    no posts handling

--- src/components/post-feed/Feed.jsx
+++ src/components/post-feed/Feed.jsx
@@ -5,6 +5,7 @@ import Post from './Post';
 import FootBar from './FootBar';
 
 function Feed({ fetchForUsername, userInfo, fetchFunction }) {
+    const fakeFetch = () => [];
     const fetchPosts = async (pageParam) => {
         const results = await fetchFunction(fetchForUsername, pageParam);
         return { results, nextPage: pageParam + 1, totalPages: 100 };
@@ -21,22 +22,24 @@ function Feed({ fetchForUsername, userInfo, fetchFunction }) {
         refetchOnWindowFocus: false,
         getNextPageParam: (lastPage) => (lastPage.nextPage < lastPage.totalPages) ? lastPage.nextPage : undefined
     });
-    
+
     return (
         <>
             {isLoading ? (
                     <p>Loading...</p>
                 ) : isError ? (
                         <p>There was an error</p>
-                    ) : (
-                        <>
-                            <InfiniteScroll hasMore={hasNextPage} loadMore={fetchNextPage}>
-                                {data.pages.map((page) =>
-                                    page.results.map((post) => <Post post={post} userInfo={userInfo} key={post.id}/>)
-                                )}
-                            </InfiniteScroll>
-                            <FootBar/>
-                        </>
+                    ) : ((!!data && !!(data.pages) && !!(data.pages[0]) && !!(data.pages[0].results) && !!(data.pages[0].results[0])) ?
+                            <>
+                                <InfiniteScroll hasMore={hasNextPage} loadMore={fetchNextPage}>
+                                    {data.pages.map((page) =>
+                                        page.results.map((post) => <Post post={post} userInfo={userInfo} key={post.id}/>)
+                                    )}
+                                </InfiniteScroll>
+                                <FootBar/>
+                            </>
+                            :
+                            <h3>No posts to display</h3>
                     )
             }
         </>
