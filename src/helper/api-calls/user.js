ca8c6d70c3c47801f47bff77b73a8c3cd554608b
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-12-06 12:03:27 -0500

    remove mockpfp

--- src/helper/api-calls/user.js
+++ src/helper/api-calls/user.js
@@ -1,9 +1,4 @@
 import Post from '../../components/post-feed/Post';
-import mockPFP from '../../logo192.png';
-
-export /*async*/ function fetchProfileInfoForUser(username) {
-    return {profilePic: mockPFP};
-}
 
 export /*async*/ function fetchFriendsForUser(username) {
     /*
@@ -19,27 +14,27 @@ export /*async*/ function fetchFriendsForUser(username) {
     */
     console.log(`fetching ${username}'s friends`);
     const mockFriendList = [
-        {key: 'Friend 1', username: 'Friend 1', profilePic: mockPFP},
-        {key: 'Friend 2', username: 'Friend 2', profilePic: mockPFP},
-        {key: 'Friend 3', username: 'Friend 3', profilePic: mockPFP},
-        {key: 'Friend 4', username: 'Friend 4', profilePic: mockPFP},
-        {key: 'Friend 5', username: 'Friend 5', profilePic: mockPFP},
-        {key: 'Friend 6', username: 'Friend 6', profilePic: mockPFP},
-        {key: 'Friend 7', username: 'Friend 7', profilePic: mockPFP},
-        {key: 'Friend 8', username: 'Friend 8', profilePic: mockPFP},
-        {key: 'Friend 9', username: 'Friend 9', profilePic: mockPFP},
-        {key: 'Friend 10', username: 'Friend 10', profilePic: mockPFP},
-        {key: 'Friend 11', username: 'Friend 11', profilePic: mockPFP},
-        {key: 'Friend 12', username: 'Friend 12', profilePic: mockPFP},
-        {key: 'Friend 13', username: 'Friend 13', profilePic: mockPFP},
-        {key: 'Friend 14', username: 'Friend 14', profilePic: mockPFP},
-        {key: 'Friend 15', username: 'Friend 15', profilePic: mockPFP},
-        {key: 'Friend 16', username: 'Friend 16', profilePic: mockPFP},
-        {key: 'Friend 17', username: 'Friend 17', profilePic: mockPFP},
-        {key: 'Friend 18', username: 'Friend 18', profilePic: mockPFP},
-        {key: 'Friend 19', username: 'Friend 19', profilePic: mockPFP},
-        {key: 'Friend 20', username: 'Friend 20', profilePic: mockPFP},
-        //{key: 'Friend 21', username: 'username', profilePic: mockPFP}
+        {key: 'Friend 1', username: 'Friend 1'},
+        {key: 'Friend 2', username: 'Friend 2'},
+        {key: 'Friend 3', username: 'Friend 3'},
+        {key: 'Friend 4', username: 'Friend 4'},
+        {key: 'Friend 5', username: 'Friend 5'},
+        {key: 'Friend 6', username: 'Friend 6'},
+        {key: 'Friend 7', username: 'Friend 7'},
+        {key: 'Friend 8', username: 'Friend 8'},
+        {key: 'Friend 9', username: 'Friend 9'},
+        {key: 'Friend 10', username: 'Friend 10'},
+        {key: 'Friend 11', username: 'Friend 11'},
+        {key: 'Friend 12', username: 'Friend 12'},
+        {key: 'Friend 13', username: 'Friend 13'},
+        {key: 'Friend 14', username: 'Friend 14'},
+        {key: 'Friend 15', username: 'Friend 15'},
+        {key: 'Friend 16', username: 'Friend 16'},
+        {key: 'Friend 17', username: 'Friend 17'},
+        {key: 'Friend 18', username: 'Friend 18'},
+        {key: 'Friend 19', username: 'Friend 19'},
+        {key: 'Friend 20', username: 'Friend 20'},
+        //{key: 'Friend 21', username: 'username'}
     ];
     return mockFriendList;
 }
