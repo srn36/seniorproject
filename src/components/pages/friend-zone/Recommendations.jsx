c6d3051fac37d1322fc686b2a5013f229ffbad3a
Z-Youssef <112342934+Zach-Youssef-zty@users.noreply.github.com>
2022-12-06 19:48:25 -0500

    comments to recommendations page

--- src/components/pages/friend-zone/Recommendations.jsx
+++ src/components/pages/friend-zone/Recommendations.jsx
@@ -1,21 +1,28 @@
+/* eslint-disable */
 import React, { useEffect, useState } from 'react';
 import { useOutletContext } from 'react-router-dom';
-import { getOutgoingRequestsForUser } from '../../../unholy-abominations/simulateFriends';
+//import { getRecommendationsForUser } from '../../../unholy-abominations/simulateFriends';
 import FriendList from '../../friend-displays/FriendList';
 
+/**
+ * This is meant to be the page that displays friend recommendations to the user.
+ * Unfortunately, we do not have the ability to recommend friends to the user using any form of data analytics.
+ * As a result, all references to this page have been commented out, 
+ * including the route that is meant to resolve to this page.
+ */
 function Recommendations(props) {
     const {userInfo} = useOutletContext();
     const [loading, setLoading] = useState(true);
-    const [outgoing, setOutgoing] = useState([]);
+    const [recommendations, setRecommendations] = useState([]);
 
     useEffect(() => {
-        const fetchoutgoing = async () => {
-            const requests = await getOutgoingRequestsForUser(userInfo.username);
-            setOutgoing(requests);
+        const fetchRecommendations = async () => {
+            const recs = await getRecommendationsForUser(userInfo.username);
+            setRecommendations(recs);
             setLoading(false);
         };
 
-        fetchoutgoing();
+        fetchRecommendations();
     }, [userInfo]);
 
     return (
@@ -24,7 +31,7 @@ function Recommendations(props) {
             :
             <>
                 <h3>Friend Recommendations</h3>
-                <FriendList friends={outgoing} type={'Standard'} userInfo={userInfo}/>             
+                <FriendList friends={recommendations} type={'Recommendations'} userInfo={userInfo}/>             
             </>
     );
 
